38cf88143efd04f201ca16bb8216587dee8b280a
Pedro Santos <donbonifacio@gmail.com>
2015-06-16 09:17:00 +0100

    Consider void segment account
    To not pollute segment's main account with dummy test data, this lib
    will choose from two segment keys and when in tests, send data to a void
    segment account.

--- test/clanhr/analytics/core_test.clj
+++ test/clanhr/analytics/core_test.clj
@@ -5,10 +5,10 @@
 (def user-id "clojure-test")
 
 (deftest smoke-test-identify
-  (core/identify user-id {:name "Clojure Test"})
-  (core/identify user-id {:userId "Clojure Test"})
-  (core/identify user-id nil)
-  (core/identify user-id {}))
+  (core/identify user-id {:name "Clojure Test"} true)
+  (core/identify user-id {:userId "Clojure Test"} true)
+  (core/identify user-id nil true)
+  (core/identify user-id {} true))
 
 (deftest smoke-test-track
-  (core/track user-id "Clojure unit test"))
+  (core/track user-id "Clojure unit test" true))
