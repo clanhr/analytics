f7ddda35f9129d47bb909895bcc595eb0a9cbdd0
Pedro Santos <donbonifacio@gmail.com>
2015-09-22 14:27:46 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clanhr/result "0.6.0"]
-                 [commons-logging "1.1.3"]
-                 [clj-time "0.9.0"]
+                 [clanhr/result "0.9.3"]
+                 [commons-logging "1.2"]
+                 [clj-time "0.11.0"]
                  [clj-bugsnag "0.2.3"]
-                 [environ "1.0.0"]
+                 [environ "1.0.1"]
                  [clj-librato "0.0.5"]
-                 [analytics-clj "0.2.2"]
+                 [analytics-clj "0.3.0"]
 
                  ;; disable analytics-clj log
                  [org.slf4j/slf4j-nop "1.7.12"]
