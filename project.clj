652c757d13143b9edb962af985238c22d29ca684
Pedro Santos <donbonifacio@gmail.com>
2016-01-29 16:57:31 +0000

    Use shared deps

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "https://github.com/clanhr/analytics"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clanhr/result "0.9.3"]
-                 [commons-logging "1.2"]
-                 [clj-time "0.11.0"]
+
+  :dependencies.edn "https://raw.githubusercontent.com/clanhr/dependencies/master/dependencies.edn"
+
+  :dependency-sets [:clojure :common :clanhr]
+
+  :dependencies [[commons-logging "1.2"]
                  [clj-bugsnag "0.2.3"]
-                 [environ "1.0.1"]
                  [clj-librato "0.0.5"]
                  [analytics-clj "0.3.0"]
 
@@ -19,4 +20,6 @@
                  ;[org.slf4j/jcl-over-slf4j "1.7.5"]
                  ;[ch.qos.logback/logback-classic "1.0.13"]
                  ;kkk[ch.qos.logback/logback-core "1.0.13"]
-                 ])
+                 ]
+
+  :plugins [[clanhr/shared-deps "0.2.6"]])
